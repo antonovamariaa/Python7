37237f9abee3de0a513cc9f5e1d1a767eccfcee4
maria <antonova.maria.a@gmail.com>
2023-09-12 13:39:36 +0300

    task1 step 2

--- task1.py
+++ task1.py
@@ -1,20 +1,23 @@
 #word = "пара-ра-рам рам-пам-папам па-ра-па-дам"
-word = input("Введит естихотворение: ")
-list1 = word.split()
-print (list1)
-counter = 0
-count_list = []
+def Input(str):
+    word = input(str)
+    list1 = word.split()
+    return list1
 
-for element in list1:
-    for char in element:
-        if char == "а":
-            counter += 1
-    count_list.append(counter)
+def Poem(list1):
     counter = 0
+    count_list = []
+    for element in list1:
+        for char in element:
+            if char == "а":
+                counter += 1
+        count_list.append(counter)
+        counter = 0
 
-count_list = set(count_list)
+    if len(set(count_list)) == 1:
+        print("Парам пам-пам")
+    else:
+        print("Пам парам")
 
-if len(count_list) == 1:
-    print("Парам пам-пам")
-else:
-    print("Пам парам")
+
+Poem(Input("Введит естихотворение: "))
